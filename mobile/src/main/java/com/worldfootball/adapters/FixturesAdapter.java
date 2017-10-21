a96d313748fc9c837938ba973eaa9ae573c2b757
VVoev <vladovoev@yahoo.co.uk>
2017-10-21 10:41:53 +0300

    added adapters

---
+++ mobile/src/main/java/com/worldfootball/adapters/FixturesAdapter.java
@@ -0,0 +1,72 @@
+package com.worldfootball.adapters;
+
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.worldfootball.R;
+import com.worldfootball.adapters.abstractions.AbstractRecyclerAdapter;
+import com.worldfootball.models.Fixture;
+import com.worldfootball.utils.UserInterface;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class FixturesAdapter extends AbstractRecyclerAdapter<Fixture, FixturesAdapter.FixturesViewHolder> {
+
+	private Resources mResources;
+
+	public FixturesAdapter(Resources resources, ArrayList<Fixture> list) {
+		super(list);
+		mResources = resources;
+	}
+
+	@Override
+	public FixturesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_fixture, parent, false);
+		return new FixturesViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(FixturesViewHolder holder, int position) {
+		Fixture item = mList.get(position);
+		holder.mHomeTeamName.setText(item.getHomeTeamName());
+		holder.mAwayTeamName.setText(item.getAwayTeamName());
+		if (item.getStatus() != Fixture.TIMED) {
+			UserInterface.hide(holder.mDate);
+			UserInterface.show(holder.mResult);
+			holder.mResult.setText(String.format(mResources.getString(
+					R.string.fixtures_list_item_result),
+					item.getGoalsHomeTeam(),
+					item.getGoalsAwayTeam()));
+		}
+		else {
+			UserInterface.hide(holder.mResult);
+			UserInterface.show(holder.mDate);
+			DateFormat format = new SimpleDateFormat("HH:mm", Locale.getDefault());
+			holder.mDate.setText(format.format(new Date(item.getDate())));
+		}
+	}
+
+	public static class FixturesViewHolder extends RecyclerView.ViewHolder {
+
+		private TextView mHomeTeamName,
+				mAwayTeamName,
+				mDate,
+				mResult;
+
+		public FixturesViewHolder(View itemView) {
+			super(itemView);
+			mHomeTeamName = (TextView) itemView.findViewById(R.id.home_team_name);
+			mAwayTeamName = (TextView) itemView.findViewById(R.id.away_team_name);
+			mDate = (TextView) itemView.findViewById(R.id.date);
+			mResult = (TextView) itemView.findViewById(R.id.result);
+		}
+	}
+}
--- mobile/src/main/java/com/worldfootball/adapters/LeaguesAdapter.java
+++ mobile/src/main/java/com/worldfootball/adapters/LeaguesAdapter.java
@@ -6,31 +6,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.worldfootball.R;
-import com.worldfootball.abstractions.AbstractRecyclerAdapter;
-import com.worldfootball.model.League;
-
+import com.worldfootball.adapters.abstractions.AbstractRecyclerAdapter;
+import com.worldfootball.contracts.IOnItemTouchAdapter;
+import com.worldfootball.models.League;
 import java.util.ArrayList;
 
-public class LeaguesAdapter extends AbstractRecyclerAdapter<League, LeaguesAdapter.LeagueViewHolder> {
+public class LeaguesAdapter extends AbstractRecyclerAdapter<League, LeaguesAdapter.LeagueViewHolder> implements IOnItemTouchAdapter {
+
+
 
 	public LeaguesAdapter(ArrayList<League> list) {
 		super(list);
 	}
 
+	public LeaguesAdapter(ArrayList<League> list, RecyclerView recyclerView) {
+		super(list, recyclerView);
+	}
+
 	@Override
 	public LeagueViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_navigation, parent, false);
+		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_league, parent, false);
 		return new LeagueViewHolder(view);
 	}
 
 	@Override
 	public void onBindViewHolder(LeagueViewHolder holder, int position) {
 		League item = mList.get(position);
-		//temp image
-		//holder.getIcon().setImageResource(R.drawable.ic_alert_circle_outline);
-		holder.getName().setText(item.getCaption());
+		String name = item.getCaption();
+			holder.mIcon.setImageResource(R.drawable.ic_alert_circle_outline);
+			holder.mName.setText(item.getCaption());
 	}
 
 	public static class LeagueViewHolder extends RecyclerView.ViewHolder {
@@ -42,14 +47,6 @@ public class LeaguesAdapter extends AbstractRecyclerAdapter<League, LeaguesAdapt
 			super(itemView);
 			mIcon = (ImageView) itemView.findViewById(R.id.icon);
 			mName = (TextView) itemView.findViewById(R.id.name);
-		}
-
-		public ImageView getIcon() {
-			return mIcon;
-		}
-
-		public TextView getName() {
-			return mName;
 		}
 	}
 
